220b799c443171f0a4e22a2b0e7dd688bcf7b0ee
dianacamacho <dcam1901@yahoo.com>
2017-02-13 14:31:29 -0600

    fill in stocks and stock components with temp data

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
     <h1>Stocks</h1>
+    <div class="row">
+      <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+    </div>
   </div>
 </template>
+
+<script>
+  import Stock from '../stocks/Stock.vue';
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 100 }, 
+          { id: 2, name: 'Google', price: 200 }, 
+          { id: 3, name: 'Apple', price: 210 },
+          { id: 3, name: 'Twitter', price: 80 },
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
+
+<style>
+  
+</style>
